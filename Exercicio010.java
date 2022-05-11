f20a7c462a60ac81aaf2a8fb285ebd7cfdee4748
retr0lbb <85702153+retr0lbb@users.noreply.github.com>
2022-05-10 23:54:37 -0300

    Update Exercicio010.java

--- Exercicio010.java
+++ Exercicio010.java
@@ -1,38 +1,32 @@
-import java.util.Calendar;
-import java.util.*;
-import java.text.SimpleDateFormat;
-public class Exercicio009 {
- public static void main(String args []) {
-	int d, m, y;
-	 
-	   Scanner input;
-	   Calendar calendar = Calendar.getInstance();
-	   SimpleDateFormat format1 = new SimpleDateFormat("dd/MM/yyyy");
-	   
+import java.time.LocalDate;
 
-	   
-	   try {
-		   System.out.print("Entre com o dia de seu nascimento: ");
+import java.util.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+class Main {
+  public static void main(String[] args) {
+    int d,m,y;
+    LocalDate nowDate = LocalDate.now();
+    Scanner input;
+      DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy ");  
+  
+    try{
+      
+		 System.out.print("Entre com o dia de seu nascimento: ");
 			input = new Scanner(System.in);
 			d = input.nextInt();
-			System.out.print("Entre com o mes do seu nascimento: ");
+      	 System.out.print("Entre com o mes de seu nascimento: ");
 			input = new Scanner(System.in);
-			m = input.nextInt()-1;
-			System.out.print("Entre com o ano do seu nascimento: ");
+			m = input.nextInt();
+      	 System.out.print("Entre com o ano de seu nascimento: ");
 			input = new Scanner(System.in);
 			y = input.nextInt();
-			calendar.set(y, m, d);
-			Date dia = calendar.getTime();
-			String finas = format1.format(dia);
-			
-			System.out.println("data = "+ finas);
-			
-
-
-			
-	   }catch (Exception error) {
-			System.out.println("Houve um erro na conversao, digite apenas caracteres numericos.");
- 
+      LocalDate d1 = LocalDate.of(y,m,d);
+      System.out.println("voce ja viveu aproximadamente: "+ChronoUnit.DAYS.between(d1, nowDate)+"dias");
+      }finally{
+      
+     }
+    }
 }
- }
-}
+  
