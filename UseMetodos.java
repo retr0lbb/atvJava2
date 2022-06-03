c5cc9ebb2b6803f512e846cdfbff5822764496a5
retr0lbb <85702153+retr0lbb@users.noreply.github.com>
2022-06-03 14:23:45 -0300

    Add files via upload

---
+++ UseMetodos.java
@@ -0,0 +1,10 @@
+
+public class UseMetodos {
+	public static void main(String[] args) {
+		SemRetorno.imprimirTexto("logaratimo");
+		SemRetorno.somar(20, 30);
+		System.out.println(MetodosComRetorno.mostrarDiaPorExtenso(5));
+		System.out.println(MetodosComRetorno.contarLetras("pabibaquigrafo"));
+	}
+
+}
